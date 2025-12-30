76369002f3a80e78b2b9471186f0126f1b3f7c21
ShrinathM-77 <mateshri21@gmail.com>
2025-12-30 21:13:46 +0530

    Fix PostgreSQL config for Railway

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,22 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST || "db",
-  user: process.env.POSTGRES_USER || "postgres",
-  password: process.env.POSTGRES_PASSWORD || "postgres",
-  database: process.env.POSTGRES_DB || "postgres",
-  port: 5432,
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+pool.on("connect", () => {
+  console.log("✅ PostgreSQL connected");
+});
+
+pool.on("error", (err) => {
+  console.error("❌ PostgreSQL error", err);
 });
 
 export default pool;
